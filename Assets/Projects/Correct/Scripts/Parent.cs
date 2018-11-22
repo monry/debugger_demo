22e48e2d8526b47aed4cd79d4852f6af0414eb4b
Tetsuya Mori <monry84@gmail.com>
2018-11-23 02:38:09 +0900

    :sparkles: Add correct project

---
+++ Assets/Projects/Correct/Scripts/Parent.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Monry.DebuggerDemo.Correct
+{
+    public class Parent : MonoBehaviour
+    {
+        [SerializeField] private GameObject monryPrefab;
+        private GameObject MonryPrefab => monryPrefab;
+
+        private void Start()
+        {
+            Spawn();
+        }
+
+        public void Spawn()
+        {
+            Instantiate(MonryPrefab, transform);
+        }
+    }
+}
---
+++ Assets/Projects/Correct/Scripts/Sample.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+namespace Monry.DebuggerDemo.Correct
+{
+    public class Sample : MonoBehaviour
+    {
+        private const float RotateAnglePerSecond = 180.0f;
+
+        private const float ScaleRatioPerSecond = 1.2f;
+
+        private const double DestroyTimer = 3.0;
+
+        private Parent Parent { get; set; }
+
+        private void Awake()
+        {
+            Parent = transform.parent.GetComponent<Parent>();
+        }
+
+        private void Start()
+        {
+            this.UpdateAsObservable()
+                .Select(_ => transform)
+                .Subscribe(
+                    x =>
+                    {
+                        x.Rotate(
+                            Vector3.forward,
+                            RotateAnglePerSecond * Time.deltaTime
+                        );
+                        x.localScale +=
+                            Vector3.one
+                                * ScaleRatioPerSecond
+                                * Time.deltaTime;
+                    },
+                    () => Parent.Spawn()
+                );
+            Observable
+                .Timer(TimeSpan.FromSeconds(DestroyTimer))
+                .Subscribe(_ => Destroy(gameObject));
+        }
+    }
+}
